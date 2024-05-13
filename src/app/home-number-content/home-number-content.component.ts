d7712208f8973ae86d3a3070b6997b1f9dfb88cd
Damian Pasterz <damian.pasterz@gmail.com>
2024-05-13 16:14:53 +0200

    add caruselle and favicon

--- src/app/home-number-content/home-number-content.component.ts
+++ src/app/home-number-content/home-number-content.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 
 @Component({
   selector: 'app-home-number-content',
   templateUrl: './home-number-content.component.html',
-  styleUrls: ['./home-number-content.component.scss']
+  styleUrls: ['./home-number-content.component.scss'],
+  standalone: true,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class HomeNumberContentComponent {
-
-}
+export class HomeNumberContentComponent {}
